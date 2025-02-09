689b0a96e2d7f3956c434300388e3a44fe2588c6
Reda daghouj <mohamedredadaghouj@gmail.com>
2025-02-09 23:54:54 +0100

    init

--- push_swap.c
+++ push_swap.c
@@ -6,35 +6,47 @@
 /*   By: mdaghouj <mdaghouj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/05 10:24:12 by mdaghouj          #+#    #+#             */
-/*   Updated: 2025/02/06 12:21:45 by mdaghouj         ###   ########.fr       */
+/*   Updated: 2025/02/09 23:53:46 by mdaghouj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	main(int argc, char *argv[])
+int ft_count_word(int argc, char **argv)
 {
-	int i;
-	int j;
+	int len;
+	int i, j;
 
-	i = 1;
+	i = 0, len = 0;
 	while (i < argc)
-	{
+	{ // "5 9" " 7 " 8 
+		if (is_space(argv[i], ))
 		j = 0;
-		while (argv[i][j] != '\0')
+		while (argv[i][j])
 		{
-			if (argv[i][j] == ' ')
-				printf('found space\n');
+			len++;
 			j++;
 		}
 		i++;
-		// j = 0;
-		// while (argv[i][j] != '\0')
-		// {
-		// 	// if (argv[i][j] >= '0' && argv[i][j] <= '9')
-				
-		// 	// 5 "6" 8 -1 "9 7" " " 4-2 004
-		// }
+		len++;
+	}
+}
+
+int	main(int argc, char *argv[])
+{
+	int i; // "6 8  "
+	char *str;
+	int len;
+
+	i = 1;
+	len = ft_count_word(argc - 1, argv[1]);
+	str = (char)malloc(sizeof(char) * len);
+	while (i < argc)
+	{
+		// str[i] = '';
 	}
 	return (0);
 }
+
+// 0 - 15 --> 5 - 100 elements
+// 0 - 35 --> +101 elements
