1c84aae5b7c5974d5e184bbd9cac0c48d8bc28f9
Reda daghouj <mohamedredadaghouj@gmail.com>
2025-02-20 20:24:19 +0100

    fix leak

--- bonus/gnl_bonus/get_next_line_utils_bonus.c
+++ bonus/gnl_bonus/get_next_line_utils_bonus.c
@@ -6,7 +6,7 @@
 /*   By: mdaghouj <mdaghouj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/15 10:24:31 by mdaghouj          #+#    #+#             */
-/*   Updated: 2025/02/20 18:53:53 by mdaghouj         ###   ########.fr       */
+/*   Updated: 2025/02/20 20:22:21 by mdaghouj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,19 +62,26 @@ char	*ft_strjoin(char *s1, char *s2)
 	int		i;
 	int		j;
 
-	if (!s1 && !s2)
-		return (NULL);
 	i = 0;
 	j = 0;
-	str = malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
-	if (!str)
+	if (!s1 && !s2)
 		return (NULL);
-	while (s1 && s1[j] != '\0')
-		str[i++] = s1[j++];
-	j = 0;
-	while (s2 && s2[j] != '\0')
-		str[i++] = s2[j++];
-	str[i] = '\0';
+	else if (!s1 && s2)
+		return (ft_strdup(s2));
+	else if (s1 && !s2)
+		return (ft_strdup(s1));
+	else
+	{
+		str = malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
+		if (!str)
+			return (NULL);
+		while (s1[j])
+			str[i++] = s1[j++];
+		j = 0;
+		while (s2[j])
+			str[i++] = s2[j++];
+		str[i] = '\0';
+	}
 	return (str);
 }
 
